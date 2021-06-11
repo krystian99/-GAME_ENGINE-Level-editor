5ce03b7f1ac147a2b865774f74ef41e1d1a83a18
krystian99 <skylerr091@gmail.com>
2021-06-11 23:35:02 +0200

    Tworzenie klasy sterującej zdarzeniami Event_handler

---
+++ [GAME_ENGINE] Level editor/Event_handler.cpp
@@ -0,0 +1,6 @@
+#include "Event_handler.h"
+
+void Event_handler::push(FUNCTION& f)
+{
+	events.push(f);
+}
---
+++ [GAME_ENGINE] Level editor/Event_handler.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <queue>
+#include <SDL_events.h>
+
+using namespace std;
+
+class Event_handler
+{
+	using FUNCTION = void(*)();
+public:
+	void push(FUNCTION & f);
+private:
+	queue<FUNCTION &> events;
+	SDL_Event event_handler;
+};
