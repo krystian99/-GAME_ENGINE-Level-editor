e03a5ba96febd308c2100ef10855345d19a78b5d
krystian99 <skylerr091@gmail.com>
2021-06-18 13:52:58 +0200

    drobne poprawki projektowe

--- [GAME_ENGINE] Level editor/Engine_manager.h
+++ [GAME_ENGINE] Level editor/Engine_manager.h
@@ -8,14 +8,11 @@
 
 class Engine_manager
 {
+	friend class Engine;
 public:
-	static void INIT(MENU* menu, LevelCreator* lc, Animation_management* am, Script_Editor* se);
-
 	static void Quit();
 
 	static const Engine_state & getState() { return state; }
-
-	static Module_base* getModule() { return currentModule; }
 
 	static const bool & is_running() { return running; }
 
@@ -23,6 +20,9 @@ public:
 
 	static void set_updateState(Engine_updateState state);
 	static Engine_updateState get_updateState() { return update_state; }
+private:
+	static void INIT(MENU* menu, LevelCreator* lc, Animation_management* am, Script_Editor* se);
+	static Module_base* getModule() { return currentModule; }
 private:
 	static Engine_state state;
 	static Engine_updateState update_state;
